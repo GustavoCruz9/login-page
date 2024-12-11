30624f458ac3e8adee25fae2548db1927834a1e4
GustavoCruz9 <gustavocangaiba83@gmail.com>
2024-12-10 22:56:07 -0300

    fix: animação da sidebar funcionando

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -4,21 +4,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   selector: 'app-default-login-layout',
   imports: [],
   templateUrl: './default-login-layout.component.html',
-  styleUrl: './default-login-layout.component.scss'
+  styleUrl: './default-login-layout.component.scss',
 })
 export class DefaultLoginLayoutComponent {
- @Input() title:string = "";
- @Input() primaryBtnText:string = "";
- @Input() secondaryBtnText:string = "";
- @Input() disablePrimaryBtn:boolean = true;
- @Output("submit") onSubmit = new EventEmitter();
- @Output("navigate") onNavigate = new EventEmitter();
+  @Input() title: string = '';
+  @Input() primaryBtnText: string = '';
+  @Input() secondaryBtnText: string = '';
+  @Input() disablePrimaryBtn: boolean = true;
+  @Output('submit') onSubmit = new EventEmitter();
+  @Output('navigate') onNavigate = new EventEmitter();
 
- submit() {
-  this.onSubmit.emit();
- }
+  submit() {
+    this.onSubmit.emit();
+  }
 
- navigate() {
-  this.onNavigate.emit();
- }
+  navigate() {
+    this.onNavigate.emit();
+  }
 }
