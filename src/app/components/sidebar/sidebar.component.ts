77a601b04c8d2cc9d07caabe79264e3c2bf88400
GustavoCruz9 <gustavocangaiba83@gmail.com>
2024-12-10 22:58:16 -0300

    fix: animação da sidebar funcionando

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,27 +1,26 @@
-import { Component } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   standalone: true,
-  imports: [
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-  ],
 })
 export class SidebarComponent {
-  // Variável para controlar o estado da sidebar
-  isSidebarClosed = true;
+  isClosed = true; // Controla o estado da sidebar
 
-  // Método para alternar entre aberto e fechado
-  toggleSidebar(): void {
-    this.isSidebarClosed = !this.isSidebarClosed; // Alterna entre aberto e fechado
+  constructor(private elementRef: ElementRef) {}
+
+  toggleSidebar() {
+    this.isClosed = !this.isClosed;
+  }
+
+  closeSidebar(event: Event) {
+    const clickedElement = event.target as HTMLElement;
+
+    // Verifica se o clique foi fora do sidebar ou em um link interno
+    if (!this.elementRef.nativeElement.contains(clickedElement) || clickedElement.tagName === 'A') {
+      this.isClosed = true;
+    }
   }
 }
